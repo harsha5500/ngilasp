04ab8408cc24dd57cf9fbd463d32517b5913e3a8
Bubby <source@kharsha.com>
2012-02-13 20:00:14 +0530

    Made changes according to new design. To retain only the bare minimum for the framework.The agent, behaviour has been changed accordingly. Added a new concept and class called the agent universe where agents live. Removing excess trash and simulation specific codes.

---
+++ src/Agents/Universe/Universe2D.java
@@ -0,0 +1,11 @@
+package Agents.Universe;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: bubby
+ * Date: 2/12/12
+ * Time: 11:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Universe2D {
+}
